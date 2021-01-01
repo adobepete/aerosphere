fc031d2612f9c40cd127f0145630ac46a780aba9
falco <falco@adobe.com>
2021-01-01 16:40:05 -0500

    Parse URL, code cleanup

--- s3_photoExample.js
+++ s3_photoExample.js
@@ -242,7 +242,18 @@ function playAlbum(albumName) {
       "<a class='next' onclick='plusSlides(1)'>&#10095;</a>"
     ];
     document.getElementById("app").innerHTML = getHtml(htmlTemplate);
-    showSlides();
+    var slideIndex = 0;
+
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+        
   });
 }
 
